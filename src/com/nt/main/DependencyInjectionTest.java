4027703fb2abb0fc8ea305b4385ff65de1346154
Priyanka Kumari <kumaripriyanka94528@gmail.com>
2025-07-16 00:08:50 +0530

    Season finder

--- src/com/nt/main/DependencyInjectionTest.java
+++ src/com/nt/main/DependencyInjectionTest.java
@@ -1,19 +1,27 @@
 package com.nt.main;
+import java.time.LocalDate;
+import java.util.Arrays;
 
-import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.nt.sbeans.WishMessageGenerator;
-import com.nt.sbeans.WishMessageGenerator;
+import com.nt.sbeans.SeasonFinder;
+
 public class DependencyInjectionTest {
-	
-public static void main (String [] args) {
-	 FileSystemXmlApplicationContext ctx= new FileSystemXmlApplicationContext("src\\com\\nt\\cfgs\\applicationContent.xml");
-	 Object obj= ctx.getBean("wmg");
-	 
-	 	WishMessageGenerator generator= (WishMessageGenerator)obj;
-	 	String result=generator.generatorWishMessage("Priyanka");
-	 	System.out.println(result);
-	 	ctx.close();
-	 
-}
+	public static void main(String [] args) {
+		System.out.println("DependencyInjectionTest.main() (start)");
+		try(ClassPathXmlApplicationContext ctx=new ClassPathXmlApplicationContext 
+				("com/nt/cfgs/applicationContext.xml")){
+		SeasonFinder finder=ctx.getBean("sf",SeasonFinder.class);
+		
+	String msg=finder.showSeasonName();
+		System.out.print(msg);	System.out.println("-------------------");
+		System.out.println("Spring Beans count:: "+ctx.getBeanDefinitionCount());
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("DependencyInjectionTest.main()(end)");
+		
+	}
+
 }
