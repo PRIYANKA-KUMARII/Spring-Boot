848ce826a7188babe43b9520ceb09647a38a5297
Priyanka Kumari <kumaripriyanka94528@gmail.com>
2025-10-23 12:16:51 +0530

    MVC Project

---
+++ src/test/java/com/nit/MvcDay1TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcDay1TaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
